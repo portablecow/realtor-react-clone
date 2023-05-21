fa8d2dd76b54800abd103c4c8e3d601e37dc7bf4
Matt Patella <mfpatella@msn.com>
2023-05-21 07:06:24 -0600

    added forgot password email functionality

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 
 import { Link } from 'react-router-dom';
 import OAuth from '../components/OAuth';
+import { toast } from 'react-toastify';
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
 
 export default function ForgotPassword() {
   
@@ -9,6 +11,17 @@ export default function ForgotPassword() {
   
   function onChange(e) {
     setEmail(e.target.value);
+  }
+
+  async function onSubmit(e) {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      await sendPasswordResetEmail(auth, email);
+      toast.success("Email was sent");
+    } catch (error) {
+      toast.error("Could not send rest link");
+    }
   }
   return (
     <section>
@@ -19,7 +32,7 @@ export default function ForgotPassword() {
           alt="bear farts" className='w-full rounded-2xl'/>
         </div>
         <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
-          <form >
+          <form onSubmit={onSubmit}>
             <input className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white 
             border-gray-300 rounded transition ease-in-out" type="email" id='email'
             value={email} onChange={onChange} placeholder='Email Address' />
